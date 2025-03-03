7164d263f827f2e8a670e28b6a3d0870cba4a8dd
thiagocrepequer <thiago.crepequer@hotmail.com>
2025-03-03 15:15:18 -0300

    improvement: add if exists parameter

--- src/main/resources/db/migration/V15__DB.sql
+++ src/main/resources/db/migration/V15__DB.sql
@@ -4,24 +4,24 @@ ALTER TABLE email_code
 DROP TABLE IF EXISTS food_order_foods CASCADE;
 
 ALTER TABLE analytic_company_access
-    DROP COLUMN company_id;
+    DROP COLUMN IF EXISTS company_id;
 
 ALTER TABLE discount
-    DROP COLUMN company_id;
+    DROP COLUMN IF EXISTS company_id;
 
 ALTER TABLE discount
-    DROP COLUMN status;
+    DROP COLUMN IF EXISTS status;
 
 ALTER TABLE food
-    DROP COLUMN company_id;
+    DROP COLUMN IF EXISTS company_id;
 
 ALTER TABLE food_order
-    DROP COLUMN company_id;
+    DROP COLUMN iF EXISTS company_id;
 
 ALTER TABLE refresh_token
-    DROP COLUMN tenant_id;
+    DROP COLUMN IF EXISTS tenant_id;
 
 ALTER TABLE email_code
-    DROP COLUMN user_id;
+    DROP COLUMN IF EXISTS user_id;
 
 DROP SEQUENCE IF EXISTS analytic_company_access_seq CASCADE;

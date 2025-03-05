5dafefc21456c3cd7472cd4555bc8c090fd435b0
thiagocrepequer <thiago.crepequer@hotmail.com>
2025-03-05 17:52:00 -0300

    improvement: adjust cors

--- src/main/java/net/mymenu/config/WebSocketConfig.java
+++ src/main/java/net/mymenu/config/WebSocketConfig.java
@@ -1,8 +1,11 @@
 package net.mymenu.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://api.my-menu.net", corsAllowedOrigins)
+                .setAllowedOrigins(corsAllowedOrigins)
                 .withSockJS();
     }
 
@@ -27,5 +30,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic", "/user/queue");
         registry.setUserDestinationPrefix("/user");
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/ws/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .allowedOrigins(corsAllowedOrigins);
+            }
+        };
     }
 }
